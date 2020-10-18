4ab5296e33be36fd906d867667b79fef29f2d47c
Samuel Lord <sllord@icloud.com>
2020-10-17 19:25:53 -0500

    Update betterify.bat

--- betterify.bat
+++ betterify.bat
@@ -1,10 +1,21 @@
+:: Bringing back F8 menu
+echo Bringing back the F8 menu...
+bcdedit /set {default} bootmenupolicy legacy
+
 :: Disable Cortana
+echo Disabling Cortana and Search
 taskkill /F /IM SearchUI.exe
 rename "C:\Windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy" "C:\Windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy.bak" > NUL 2>&1
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v "SearchboxTaskbarMode" /t REG_DWORD /d 0 /f > NUL 2>&1
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /t REG_DWORD /d 0 /f > NUL 2>&1
 
+:: Disabling Oklomsy Brrr mark
+echo Disabling Windows Watermark
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v "PaintDesktopVersion" /t REG_DWORD /d 0 /f > NUL 2>&1
+sc config sppsvc start=disabled
+
 ::Add UTC Time
+echo Change to UTC time
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /t REG_DWORD /d 1 /f > NUL 2>&1
 
 :: Disable crap services and SMBv1
@@ -12,8 +23,8 @@ echo Disable crap services and SMBv1, therefore lowering chance people on this P
 sc config diagtrack start=disabled
 sc config RetailDemo start=disabled
 PowerShell -Command "Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force"
-sc config lanmanworkstation depend= bowser/mrxsmb20/nsi
-sc config mrxsmb10 start= disabled
+sc config lanmanworkstation depend=bowser/mrxsmb20/nsi
+sc config mrxsmb10 start=disabled
 
 :: Disable + Delete Tasks
 echo Disabling tasks and Windows Updates...
@@ -21,7 +32,12 @@ schtasks /delete /TN "\Microsoft\Windows\Windows Defender\Windows Defender Sched
 net stop wuauserv
 sc config wuauserv start=disabled
 
+:: Installing alternative apps
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "choco install -y --force --allow-empty-checksums vlc 7zip open-shell jpegview vcredist-all directx firefox obs onlyoffice"
+
 :: Disable Telemetry
+echo Disabling Telemetry...
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f > NUL 2>&1
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v AllowTelemetry /t REG_DWORD /d 0 /f > NUL 2>&1
 
@@ -37,6 +53,28 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AlwaysUseAu
 echo "" > C:\ProgramData\Microsoft\Diagnosis\ETLLogs\AutoLogger\AutoLogger-Diagtrack-Listener.etl
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\WMI\AutoLogger\AutoLogger-Diagtrack-Listener" /v "Start" /t REG_DWORD /d 0 /f > NUL 2>&1
 
+:: Editing HOSTS
+echo Editing HOSTS
+echo. > %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0001.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0002.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0003.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0004.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0005.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0006.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0007.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0008.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	a-0009.a-msedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	ads.mopub.com >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	ads.msn.com >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	ads.msn.com >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	ads.msn.com >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	telemetry.microsoft.com >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	akamaiedge.net >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	feedback.windows.com >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	feedback.search.microsoft.com >> %WINDIR%\System32\drivers\etc\hosts
+echo 0.0.0.0	feedback.microsoft-hohm.com >> %WINDIR%\System32\drivers\etc\hosts
 
-
-
+:: Deleting all apps except store
+echo Deleting all apps except store
+PowerShell -Command "Get-AppxPackage -AllUsers | where-object {$_.name –notlike '*store*'} | Remove-AppxPackage"
