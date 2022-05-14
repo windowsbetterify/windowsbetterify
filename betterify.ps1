b2123fcb251949e1db8e9bc2c0b9eb29f020621b
Samuel Lord <sam@sllord.info>
2022-05-14 09:28:18 -0700

    Update betterify.ps1

--- betterify.ps1
+++ betterify.ps1
@@ -1,24 +1,20 @@
 title Windows Betterify by Windows Better Team
 cls
 
-:: Bringing back F8 menu
 echo Bringing back the F8 menu...
 bcdedit /set {default} bootmenupolicy legacy
 
-:: Disable Cortana
 echo Disabling Cortana and Search
 taskkill /F /IM SearchUI.exe
 rename 'C:\Windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy' 'C:\Windows\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy.bak' > NUL 2>&1
 reg add 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Search' /v 'SearchboxTaskbarMode' /t REG_DWORD /d 0 /f > NUL 2>&1
 reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search' /v 'AllowCortana' /t REG_DWORD /d 0 /f > NUL 2>&1
 
-:: Disabling Mr Brrr mark
 echo Disabling Watermark
 sc config sppsvc start=disabled
 reg add 'HKEY_CURRENT_USER\Control Panel\Desktop' /v 'PaintDesktopVersion' /t REG_DWORD /d 0 /f > NUL 2>&1
 schtasks /delete /TN '\Microsoft\Windows\Clip\License Validation' /f > NUL 2>&1
 
-:: Disable crap services and SMBv1
 echo Disable crap services and SMBv1, therefore lowering chance people on this PC would get an EternalBlue virus.
 sc config diagtrack start=disabled
 sc config RetailDemo start=disabled
@@ -37,10 +33,8 @@ sc config RemoteRegistry start=disabled
 sc config lanmanworkstation depend=bowser/mrxsmb20/nsi
 sc config mrxsmb10 start=disabled
 
-:: Disable Timeline
 reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\System' /v 'EnableActivityFeed' /t REG_DWORD /d 0 /f > NUL 2>&1
 
-:: Disable + Delete Tasks
 echo Disabling task...
 schtasks /change /TN '\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser' /DISABLE > NUL 2>&1
 schtasks /change /TN '\Microsoft\Windows\Application Experience\ProgramDataUpdater' /DISABLE > NUL 2>&1
@@ -54,7 +48,6 @@ schtasks /delete /TN '\Microsoft\Windows\Application Experience\StartupAppTask' 
 schtasks /delete /TN '\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip' /f > NUL 2>&1
 schtasks /delete /TN '\Microsoft\Windows\Maps\MapsToastTask' /f > NUL 2>&1
 
-:: Disable Telemetry
 echo Disabling Telemetry...
 reg add 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection' /v AllowTelemetry /t REG_DWORD /d 0 /f > NUL 2>&1
 reg add 'HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection' /v AllowTelemetry /t REG_DWORD /d 0 /f > NUL 2>&1
@@ -79,7 +72,6 @@ Disable-ScheduledTask -TaskName 'Microsoft\Windows\Application Experience\Micros
 Disable-ScheduledTask -TaskName 'Microsoft\Windows\Application Experience\ProgramDataUpdater' | Out-Null
 Disable-ScheduledTask -TaskName 'Microsoft\Windows\Autochk\Proxy' | Out-Null
 
-:: RegEdits
 echo Applying Registry Edits
 reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Error Reporting' /v DontSendAdditionalData /t REG_DWORD /d 1 /f > NUL 2>&1
 reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search' /v AllowCortana /t REG_DWORD /d 0 /f > NUL 2>&1
@@ -116,18 +108,14 @@ reg add 'HKLM\SOFTWARE\Microsoft\Speech_OneCore\Preferences' /v 'VoiceActivation
 reg add 'HKLM\SOFTWARE\Microsoft\Speech_OneCore\Preferences' /v 'ModelDownloadAllowed' /t REG_DWORD /d 0 /f > NUL 2>&1
 reg add 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE' /v 'DisableVoice' /t REG_DWORD /d 1 /f > NUL 2>&1
 
-:: Only use security updates
 echo Disabling all updates except security updates...
 curl 'https://raw.githubusercontent.com/windowsbetterify/windowsbetterify/main/security-updates-only.reg' -o security-updates-only.reg
 regedit /s security-updates-only.reg
 
-:: Better UX
 echo Better UX by neolectron
 curl 'https://raw.githubusercontent.com/windowsbetterify/windowsbetterify/main/improved-experience-neolectron.reg' -o security-updates-only.re
 regedit /s improved-experience-neolectron.reg
 
-
-:: Editing HOSTS
 echo Editing HOSTS
 attrib -R %WINDIR%\System32\drivers\etc\hosts
 echo. > %WINDIR%\System32\drivers\etc\hosts
@@ -157,14 +145,11 @@ echo 0.0.0.0	sls.update.microsoft.com.akadns.net >> %WINDIR%\System32\drivers\et
 takeown /f '%WINDIR%\System32\drivers\etc\hosts'
 attrib +R %WINDIR%\System32\drivers\etc\hosts
 
-:: Disable Hibernation, to make NTFS available in other OSes
 powercfg /h off
 
-:: Deleting all apps except store
-echo Deleting all bad apps except store
-Get-AppxPackage -AllUsers | where-object {$_.name –notlike "*store*"} | Remove-AppxPackage
+echo Deleting all bad apps except store and XBOX
+Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*Microsoft.WindowsStore*"} | where-object {$_.name -notlike "*Microsoft.WindowsCalculator*"} | where-object {$_.name -notlike "*Microsoft.Windows.Photos*"}  | where-object {$_.name -notlike "*xbox*"} | Remove-AppxPackage
 
-:: Disable app suggestions
 echo Disabling app suggestions
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Name 'OemPreInstalledAppsEnabled' -Type DWord -Value 0
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Name 'SubscribedContent-353698Enabled' -Type DWord -Value 0
@@ -177,11 +162,9 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentD
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Name 'SubscribedContent-338388Enabled' -Type DWord -Value 0
 Set-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\ContentDeliveryManager' -Name 'SubscribedContent-338389Enabled' -Type DWord -Value 0
 
-:: Disable tailored experience
 echo Disable tailored experiences
 reg add 'HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\CloudContent' /v DisableTailoredExperiencesWithDiagnosticData /t REG_DWORD /d 1 /f > NUL 2>&1
 
-:: Disable SmartScreen etc (imo its scareware, because it falsely detects some apps!)
 echo Disabling SmartScreen etc.
 reg add 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer' /v SmartScreenEnabled /t REG_SZ /d 'Off' /f > NUL 2>&1
 reg add 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost' /v ContentEvaluation /t REG_DWORD /d 0 /f > NUL 2>&1
@@ -190,11 +173,6 @@ reg delete 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupAppro
 reg add 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments' /v SaveZoneInformation /t REG_DWORD /d 1 /f > NUL 2>&1
 reg add 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Attachments' /v SaveZoneInformation /t REG_DWORD /d 1 /f > NUL 2>&1
 
-:: Clean up control panel
-echo Cleaning control panel...
-reg add 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer' /v SettingsPageVisibility /t REG_SZ /d 'showonly:display;nightlight;sound;notifications;quiethours;powersleep;batterysaver;tabletmode;multitasking;clipboard;remote-desktop;about;bluetooth;connecteddevices;printers;mousetouchpad;devices-touchpad;typing;pen;autoplay;usb;network-status;network-cellular;network-wifi;network-wificalling;network-wifisettings;network-ethernet;network-dialup;network-vpn;network-airplanemode;network-mobilehotspot;datausage;network-proxy;personalization-background;personalization-start;fonts;colors;lockscreen;themes;taskbar;defaultapps;videoplayback;startupapps;dateandtime;regionlanguage;regionformatting;gaming;gaming-gamemode;gaming-broadcasting;gaming-gamedvr;easeofaccess-display;easeofaccess-colorfilter;easeofaccess-audio;easeofaccess-easeofaccess-narrator;easeofaccess-magnifier;easeofaccess-highcontrast;easeofaccess-closedcaptioning;yourinfo;emailandaccounts;workplace;signinoptions;speech;easeofaccess-speechrecognition;easeofaccess-eyecontrol;easeofaccess-keyboard;easeofaccess-mouse;cortana-windowssearch;search-moredetails;windowsupdate;windowsupdate-action;windowsupdate-history;windowsupdate-restartoptions;windowsupdate-options;windowsdefender;recovery;activation;holographic;holographic-audio;privacy;privacy-location;privacy-webcam;privacy-microphone;privacy-notifications;privacy-speechtyping;privacy-accountinfo;privacy-contacts;privacy-calandar;privacy-callhistory;privacy-email;privacy-tasks;privacy-messaging;privacy-radios;privacy-customdevices;privacy-feedback;privacy-backgroundapps;privacy-appdiagnostics;otherusers;sync;optionalfeatures;appsfeatures' /f > NUL 2>&1
-
-:: Set up explorer
 echo Setting up explorer
 reg delete 'HKEY_CLASSES_ROOT\CABFolder\CLSID' /f > NUL 2>&1
 reg delete 'HKEY_CLASSES_ROOT\SystemFileAssociations\.cab\CLSID' /f > NUL 2>&1
@@ -206,13 +184,11 @@ reg add 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' /v La
 reg add 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' /v HideFileExt /t REG_DWORD /d 0 /f > NUL 2>&1
 reg add 'HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' /v Hidden /t REG_DWORD /d 1 /f > NUL 2>&1
 
-:: Run OOShutUp
 echo 'Running ShutUp10++'
 curl 'https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe' -o OOSU10.exe
 curl 'https://raw.githubusercontent.com/windowsbetterify/windowsbetterify/main/ooshutup10.cfg' -o ooshutup10.cfg
 ./OOSU10.exe ooshutup10.cfg /quiet
 
-::Change default TTL for unlimited tethering
 echo Changing TTL for limitless tethering on an unlimited plan...
 netsh int ipv4 set glob defaultcurhoplimit=65
 netsh int ipv6 set glob defaultcurhoplimit=65
